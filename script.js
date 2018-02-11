ca066d2c06e5a73ffd91c65a6cf49d9aa71fea11
Aries945 <lisfox94@mail.ru>
2018-02-11 14:24:34 +0300

    132

--- script.js
+++ script.js
@@ -17,3 +17,40 @@ for(d=0; d<60; ++d){
   line.lineTo(qX,qY);
 }
 conext.stroke(line);
+
+function drawWatch(){
+  var date = new Date(), h, min, sec;
+  h=date.getHours();
+  min=date.getMinutes();
+  sec=date.getSeconds();
+  secAngle = (sec/60)*(2*Math.PI);
+  minAngle = (min/60)*(2*Math.PI);
+  hAngle = (h%12/12)*(2*Math.PI);
+  secAngle = Math.PI/2-secAngle;
+  minAngle = Math.PI/2-minAngle;
+  hAngle = Math.PI/2-hAngle;
+
+  var sX, sY;
+  sX=Math.cos(secAngle)*R+R;
+  sY=-Math.sin(secAngle)*R+R;
+  line.moveTo(sX,sY);
+  line.lineTo(R,R);
+  conext.stroke(line);
+
+  var mX, mY;
+  mX=Math.cos(minAngle)*R+R;
+  mY=-Math.sin(minAngle)*R+R;
+  line.moveTo(mX,mY);
+  line.lineTo(R,R);
+  conext.stroke(line);
+
+  var hX, hY;
+  hX=Math.cos(hAngle)*R+R;
+  hY=-Math.sin(hAngle)*R+R;
+  line.moveTo(hX,hY);
+  line.lineTo(R,R);
+  conext.stroke(line);
+
+  setTimeout(drawWatch, 1000);
+}
+drawWatch();
