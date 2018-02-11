20324ea737d6ec3fe90f95e8902bfdd82627310c
Aries945 <lisfox94@mail.ru>
2018-02-11 14:52:14 +0300

    132

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 var canvas = document.getElementById('sandbox');
 var conext =canvas.getContext("2d");
-var square = new Path2D();
-var line = new Path2D();
-var R = 150, d, angle, pX, pY, qX, qY;
-for(d=0; d<60; ++d){
-  angle=(d/60)*2*(Math.PI);
-  pX=Math.cos(angle)*R;
-  pY=-Math.sin(angle)*R;
-  qX=0.9*pX;
-  qY=0.9*pY;
-  pX+=R;
-  pY+=R;
-  qX+=R;
-  qY+=R;
-  line.moveTo(pX,pY);
-  line.lineTo(qX,qY);
-}
-conext.stroke(line);
+
 
 function drawWatch(){
+  conext.clearRect(0, 0, 300, 300);
+  var line = new Path2D();
+  var R = 150, d, angle, pX, pY, qX, qY;
+  for(d=0; d<60; ++d){
+    angle=(d/60)*2*(Math.PI);
+    pX=Math.cos(angle)*R;
+    pY=-Math.sin(angle)*R;
+    qX=0.9*pX;
+    qY=0.9*pY;
+    pX+=R;
+    pY+=R;
+    qX+=R;
+    qY+=R;
+    line.moveTo(pX,pY);
+    line.lineTo(qX,qY);
+  }
+  conext.stroke(line);
+
   var date = new Date(), h, min, sec;
   h=date.getHours();
   min=date.getMinutes();
@@ -40,8 +42,6 @@ function drawWatch(){
   var mX, mY;
   mX=Math.cos(minAngle)*R+R;
   mY=-Math.sin(minAngle)*R+R;
-  mY= 0.7 * mY;
-  mX= 0.7 * mX;
   line.moveTo(mX,mY);
   line.lineTo(R,R);
   conext.stroke(line);
@@ -49,8 +49,6 @@ function drawWatch(){
   var hX, hY;
   hX=Math.cos(hAngle)*R+R;
   hY=-Math.sin(hAngle)*R+R;
-  hX= hX * 0.5;
-  hY= hY * 0.5;
   line.moveTo(hX,hY);
   line.lineTo(R,R);
   conext.stroke(line);
